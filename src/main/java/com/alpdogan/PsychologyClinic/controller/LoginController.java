1d6390010f0c099b1627053843eefa03b5acc645
Alptuğ Doğan <alpdogan000@gmail.com>
2022-11-27 14:33:56 +0300

    Creating login controller

---
+++ src/main/java/com/alpdogan/PsychologyClinic/controller/LoginController.java
@@ -0,0 +1,27 @@
+package com.alpdogan.PsychologyClinic.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+public class LoginController {
+
+    @RequestMapping("/login")
+    public ModelAndView showLoginPage() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("login");
+        return modelAndView;
+    }
+
+}
