dfe236fc6122bbd5ae2bc73362d9dd417fa94597
Alptuğ Doğan <alpdogan000@gmail.com>
2022-12-02 15:16:25 +0300

    Update Therapist.java

--- src/main/java/com/alpdogan/PsychologyClinic/entity/Therapist.java
+++ src/main/java/com/alpdogan/PsychologyClinic/entity/Therapist.java
@@ -4,13 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.security.core.GrantedAuthority;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import java.util.ArrayList;
 import java.util.List;
 
 @Data
