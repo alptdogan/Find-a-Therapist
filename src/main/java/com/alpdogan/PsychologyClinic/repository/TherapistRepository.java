a47954aba012f7959a67558f32a326773359ebe6
Alptuğ Doğan <alpdogan000@gmail.com>
2022-12-02 11:00:28 +0300

    Update TherapistRepository.java

--- src/main/java/com/alpdogan/PsychologyClinic/repository/TherapistRepository.java
+++ src/main/java/com/alpdogan/PsychologyClinic/repository/TherapistRepository.java
@@ -1,19 +1,12 @@
 package com.alpdogan.PsychologyClinic.repository;
 
-import com.alpdogan.PsychologyClinic.entity.Clients;
 import com.alpdogan.PsychologyClinic.entity.Therapist;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface TherapistRepository extends MongoRepository<Therapist, Integer> {
 
     Therapist findById(int id);
-
-    @Query("{'Clients.id':?0}")
-    List<Clients> findClientsById(int id);
 
 }
